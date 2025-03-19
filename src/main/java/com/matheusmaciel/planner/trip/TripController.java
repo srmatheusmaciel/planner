6b4c1e098f81bc2402004d02041335ea9bcca212
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-03-19 14:21:19 -0300

    Creating put method
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/planner/trip/TripController.java
+++ src/main/java/com/matheusmaciel/planner/trip/TripController.java
@@ -11,9 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/trips")
@@ -44,4 +47,21 @@ public class TripController {
          .orElseGet(() -> ResponseEntity.notFound().build());
         
      }
+
+     @PutMapping("/{id}")
+     public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload){
+         Optional<Trip> trip = this.tripRepository.findById(id);
+
+         if(trip.isPresent()){
+            Trip rawTrip = trip.get();
+            rawTrip.setDestination(payload.destination());
+            rawTrip.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            this.tripRepository.save(rawTrip);
+            return ResponseEntity.ok().body(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
+                                     
+    }
 }
